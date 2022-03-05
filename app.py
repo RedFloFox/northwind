579c006dd6855f6d83601a5ae113e1cf0a7f691d
Landon Campbell <43715591+landon-campbell@users.noreply.github.com>
2022-03-05 12:58:17 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from itertools import product
 from flask import Flask, render_template
 import database
 
@@ -5,5 +6,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    suppliers = database.get_supplier_facts()
+    suppliers = database.get_all_suppliers()
     return render_template('index.html', suppliers=suppliers)
+
+@app.route("/suppliers/<int:supplier_id>")
+def products(supplier_id):
+    products = database.get_supplier_products(supplier_id)
+    product_supplier = database.get_supplier_name(supplier_id)[0]['CompanyName']
+    return render_template('products.html', products=products, product_supplier=product_supplier)
